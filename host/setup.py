3353728bf8febc3781aca6f449e238e9ac2b99bf
Kate J. Temkin <k@ktemkin.com>
2019-03-07 20:04:35 -0700

    pygreat: handle versioning correctly, in preparation for GreatFET release

--- host/setup.py
+++ host/setup.py
@@ -6,16 +6,30 @@ def read(fname):
     with open(filename, 'r') as f:
         return f.read()
 
+
+setup_req = []
+version_format = None
+version = None
+
+# Deduce version, if possible.
+if os.path.isfile('../VERSION'):
+    version = read('../VERSION')
+else:
+    version_format = '{tag}.dev{commitcount}+git.{gitsha}'
+    setup_req.append('setuptools-git-version')
+
 setup(
     name='pygreat',
-    version='0.0', #TODO: Derive this from the main module.
+    version=version,
+    version_format=version_format,
+    setup_requires=setup_req,
     url='https://greatscottgadgets.com/greatfet/',
     license='BSD',
     entry_points={
         'console_scripts': [],
     },
     author='Katherine J. Temkin',
-    author_email='ktemkin@insomniasec.io',
+    author_email='ktemkin@greatscottgadgets.com',
     tests_require=[''],
     install_requires=['pyusb', 'future', 'backports.functools_lru_cache'],
     description='Python library for talking with libGreat devices',
