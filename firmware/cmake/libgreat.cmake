7456a69c4706c2563a63e51615a500d272a161c4
Kate J. Temkin <k@ktemkin.com>
2019-04-04 15:11:48 -0600

    firmware: cmake: provide the capability to rely on per-module extra includes

--- firmware/cmake/libgreat.cmake
+++ firmware/cmake/libgreat.cmake
@@ -35,6 +35,7 @@ function(add_flash_executable EXECUTABLE_NAME)
 	# ... and set its default properties.
 	generate_linker_script_arguments(FLAGS_LINKER_SCRIPTS ${LINKER_SCRIPTS_MAIN_CPU} ${LINKER_SCRIPT_FLASH})
 	target_link_options(${EXECUTABLE_NAME}.elf PRIVATE
+		${FLAGS_PLATFORM}
 		${FLAGS_ARCHITECTURE}
 		${FLAGS_MAIN_CPU}
 		${FLAGS_LINK_BOARD}
@@ -72,7 +73,7 @@ function(add_libgreat_library LIBRARY_NAME)
 		${PATH_LIBGREAT_FIRMWARE}/include
 		${PATH_LIBGREAT_FIRMWARE_PLATFORM}/include
 	)
-	target_compile_options(${LIBRARY_NAME} PRIVATE ${FLAGS_COMPILE_COMMON} ${FLAGS_ARCHITECTURE} ${FLAGS_MAIN_CPU})
+	target_compile_options(${LIBRARY_NAME} PRIVATE ${FLAGS_COMPILE_COMMON} ${FLAGS_PLATFORM} ${FLAGS_ARCHITECTURE} ${FLAGS_MAIN_CPU})
 	target_compile_definitions(${LIBRARY_NAME} PRIVATE ${DEFINES_COMMON} ${DEFINES_BOARD})
 
 endfunction(add_libgreat_library)
@@ -106,6 +107,22 @@ endfunction(define_libgreat_module)
 
 
 #
+# Provides the include paths necessary to use a given libgreat module. Will automatically be added to the include path
+# for the relevant libgreat module.
+#
+# Arguments: <module_name> [include_directories...]
+#
+function(libgreat_module_include_directories MODULE_NAME)
+
+	# Adds the relevant include direcotires to the given libgreat module. These will also be added to the include path
+	# of anything that uses the relevant module.
+	target_include_directories(libgreat_module_${MODULE_NAME} PUBLIC ${ARGN})
+
+endfunction(libgreat_module_include_directories)
+
+
+
+#
 # Function that adds a libgreat module to a given target.
 # Arguments: <target_to_add_to> <module_to_add> [additional_modules...]
 #
@@ -129,6 +146,12 @@ function(use_libgreat_modules TARGET_NAME)
 		# ... and include the module's sources in the target.
 		get_target_property(MODULE_SOURCES libgreat_module_${MODULE} SOURCES)
 		target_sources(${TARGET_NAME} PRIVATE ${MODULE_SOURCES})
+
+		# Include any includes specified for interfacing with the given target in the downstream target.
+		get_target_property(ADDITIONAL_INCLUDES libgreat_module_${MODULE} INTERFACE_INCLUDE_DIRECTORIES)
+		if (NOT ${ADDITIONAL_INCLUDES} MATCHES "-NOTFOUND$")
+			target_include_directories(${TARGET_NAME} PRIVATE ${ADDITIONAL_INCLUDES})
+		endif()
 
 	endforeach(MODULE)
 
