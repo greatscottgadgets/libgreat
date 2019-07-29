612a317c0c409c3948495b46bc80e9039f711282
Kate J. Temkin <k@ktemkin.com>
2019-07-29 14:09:00 -0600

    fw: implement more ARM SCB registers

--- firmware/platform/lpc43xx/include/drivers/arm_system_control.h
+++ firmware/platform/lpc43xx/include/drivers/arm_system_control.h
@@ -32,29 +32,113 @@ typedef volatile struct {
 	uint32_t shpr[3];
 
 	// System Handler control/status registers
-	uint32_t shcsr;
+	union {
+		uint32_t shcsr;
+		struct {
+			uint32_t memory_management_fault_active   :  1;
+			uint32_t bus_fault_active                 :  1;
+			uint32_t                                  :  1;
+			uint32_t usage_fault_active               :  1;
+			uint32_t                                  :  3;
+			uint32_t svc_enabled                      :  1;
+			uint32_t debug_monitor_active             :  1;
+			uint32_t                                  :  1;
+			uint32_t pended_svc_enabled               :  1;
+			uint32_t systick_enabled                  :  1;
+			uint32_t usage_fault_pending              :  1;
+			uint32_t memory_management_fault_pending  :  1;
+			uint32_t bus_fault_pending                :  1;
+			uint32_t svc_pending                      :  1;
+			uint32_t memory_managemnt_faults_enabled  :  1;
+			uint32_t bus_faults_enabled               :  1;
+			uint32_t usage_faults_enabled             :  1;
+			uint32_t                                  : 13;
+		};
+	};
 
 	// Configurable fault status register
 	union {
 		uint32_t cfsr;
 		struct {
-			uint16_t ufsr;
-			uint8_t  bfsr;
-			uint8_t mmfsr;
+
+			// Memory management fault status register
+			union {
+				uint8_t mmfsr;
+				struct {
+					uint8_t instruction_access     : 1;
+					uint8_t data_access            : 1;
+					uint8_t                        : 1;
+					uint8_t on_unstacking          : 1;
+					uint8_t on_stacking            : 1;
+					uint8_t fp_state_saving        : 1;
+					uint8_t                        : 1;
+					uint8_t fault_address_valid    : 1;
+				} memory_management_faults;
+			};
+
+
+			// Bus fault status register
+			union {
+				uint8_t  bfsr;
+				struct {
+					uint8_t instruction_bus     : 1;
+					uint8_t precise_data_bus    : 1;
+					uint8_t imprecise_data_bus  : 1;
+					uint8_t on_unstacking       : 1;
+					uint8_t on_stacking         : 1;
+					uint8_t fp_state_saving     : 1;
+					uint8_t                     : 1;
+					uint8_t fault_address_valid : 1;
+
+				} bus_faults;
+			};
+
+
+			// Usage fault status register
+			union {
+				uint16_t ufsr;
+
+				struct {
+					uint16_t undefined_instruction : 1;
+					uint16_t invalid_state         : 1;
+					uint16_t invalid_pc            : 1;
+					uint16_t no_coprocessor        : 1;
+					uint16_t                       : 4;
+					uint16_t unaligned_access      : 1;
+					uint16_t divide_by_zero        : 1;
+					uint16_t                       : 6;
+				} usage_faults;
+			};
+
 		} __attribute__((packed));
 	};
 
 	// Hard fault status register.
-	uint32_t hfsr;
+	union {
+		uint32_t hfsr;
+		struct {
+			uint32_t                      :  1;
+			uint32_t on_vector_table_read :  1;
+			uint32_t                      : 28;
+			uint32_t forced               :  1;
+			uint32_t debug_event          :  1;
+		} hard_faults;
+	};
 
 	// Debug fault status register.
 	uint32_t dfsr;
 
 	// Memory Management fault address register
-	uint32_t mmfar;
+	union {
+		uint32_t mmfar;
+		uint32_t memory_management_faulting_address;
+	};
 
 	// Bus Fault address register
-	uint32_t bfar;
+	union {
+		uint32_t bfar;
+		uint32_t bus_faulting_address;
+	};
 
 	// Aux Fault status register
 	uint32_t afsr;
@@ -74,21 +158,25 @@ typedef volatile struct {
 	// Instruction set attributes register.
 	const uint32_t isar[5];
 
-	RESERVED_WORDS(5);
+	RESERVED_WORDS(6);
 
 	// Coprocessor access control register.
 	// Note that the FPU is actually split between two "co-processors",
 	// so there's actually two two-bit registers that must be set identically.,
 	// We treat these as a single four-bit register.
-	struct {
-		uint32_t            : 20;
-		uint32_t fpu_access :  4;
-		uint32_t            :  8;
-	} cpacr;
+	union {
+		struct {
+			uint32_t            : 20;
+			uint32_t fpu_access :  4;
+			uint32_t            :  8;
+		} cpacr;
+		uint32_t cpacr_raw;
+	};
 
 } ATTR_PACKED arm_system_control_register_block_t;
 
-ASSERT_OFFSET(arm_system_control_register_block_t, afsr, 0x3c);
+ASSERT_OFFSET(arm_system_control_register_block_t, afsr,  0x3c);
+ASSERT_OFFSET(arm_system_control_register_block_t, cpacr, 0x88);
 
 
 /**
@@ -97,7 +185,7 @@ ASSERT_OFFSET(arm_system_control_register_block_t, afsr, 0x3c);
 typedef enum {
 	FPU_DISABLED        = 0b0000,
 	FPU_PRIVILEGED_ONLY = 0b0101,
-	FPU_FULL_ACCESS     = 0b1111,
+	FPU_FULL_ACCESS     = 0b111,
 } fpu_access_rights_t;
 
 
